e8be1431951d6f6b68c9b7b8ff29995cc828831f
Mike M <mikal815@users.noreply.github.com>
2018-06-13 01:55:06 -0400

    Reset Game function working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
-
-var randomNumber = Math.floor(Math.random() * 101) + 19;
+// Variables
 var win = 0;
 var loss = 0;
+var currentScore;
 
+var crystal1 = Math.floor(Math.random() * (11) + 1)
+var crystal2 = Math.floor(Math.random() * (11) + 1)
+var crystal3 = Math.floor(Math.random() * (11) + 1)
+var crystal4 = Math.floor(Math.random() * (11) + 1)
 
+// Reset Game function
+function gameRestart() {
+    randomNumber = Math.floor(Math.random() * (101) + 19);
+    var crystal1 = Math.floor(Math.random() * (11) + 1)
+    var crystal2 = Math.floor(Math.random() * (11) + 1)
+    var crystal3 = Math.floor(Math.random() * (11) + 1)
+    var crystal4 = Math.floor(Math.random() * (11) + 1)
+    currentScore = 0;
+}
 
-
-
-
+// Write to DOM
+$('#randomNum').html(randomNumber);
+$('#score').html(currentScore);
 
 
 
