dc60b2df10d2ae4f0aeebf305bce0ae1a33a7b3e
Mike M <mikal815@users.noreply.github.com>
2018-06-13 22:48:17 -0400

    styled page further and added if else statements in js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,31 +17,54 @@ function gameRestart() {
     var cryst3 = Math.floor(Math.random() * (11) + 1)
     var cryst4 = Math.floor(Math.random() * (11) + 1)
     currentScore = 0;
+
+    // Write to DOM
+    $('#randomNum').html(randomNumber);
+    $('.current').html(currentScore);
+    init();
 }
 
+function init() {
+    randomNumber = Math.floor(Math.random() * (101) + 19);
+    var cryst1 = Math.floor(Math.random() * (11) + 1)
+    var cryst2 = Math.floor(Math.random() * (11) + 1)
+    var cryst3 = Math.floor(Math.random() * (11) + 1)
+    var cryst4 = Math.floor(Math.random() * (11) + 1)
+    currentScore = 0;
+
+    // Write to DOM
+    $('#randomNum').html(randomNumber);
+    $('.current').html(currentScore);
+    //Give the crystal DOM elements a random number
+    $(".crystal1").attr("value", cryst1);
+    $(".crystal2").attr("value", cryst2);
+    $(".crystal3").attr("value", cryst3);
+    $(".crystal4").attr("value", cryst4);
+
+    $(".elem").on("click", function(){
+        var grab = $(this).attr("value");
+
+        currentScore = parseInt(currentScore) + parseInt(grab);
+        $(".current").html(currentScore);
+
+        if(randomNumber === currentScore){
+            win++
+            $("#wins").html(win);
+            $(".current").html(0);
+            gameRestart();
+        } 
+        else if (currentScore > randomNumber){
+            loss++
+            $("#losses").html(loss);
+            $(".current").html(0);
+            gameRestart();
+        }
+    });
+    
+    
+
+}
 gameRestart();
-
-// Write to DOM
-$('#randomNum').html(randomNumber);
-$('.current').html(currentScore);
-
-
-$(".crystal1").attr("value", cryst1);
-$(".crystal2").attr("value", cryst2);
-$(".crystal3").attr("value", cryst3);
-$(".crystal4").attr("value", cryst4);
-
-var grab = $(this).attr("value");
-
-
-
-
-
-
-
-
-
-
 
 
 
